70e2ed1f4eede0d00557ce1f8bbd6329dded1ec3
Aakash Rayate <rayate2410@gmail.com>
2016-08-21 11:40:24 +0530

    Initial blog implemented

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Blog(models.Model):
+	author = models.ForeignKey('auth.User')
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	publish_date = models.DateTimeField(blank=True, null=True)
+
+	def __str__(self):
+		return self.title
